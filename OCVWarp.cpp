be50eaa9a36309b33e37bcf766024c9bd564c41d
hn-88 <hn-88@users.noreply.github.com>
2024-04-05 17:20:30 +0530

    checking last_not_of

--- OCVWarp.cpp
+++ OCVWarp.cpp
@@ -975,6 +975,7 @@ int main(int argc,char *argv[])
 		// https://stackoverflow.com/questions/538300/check-what-number-a-string-ends-with-in-c
 		std::string test = OpenFileName;
 		std::string::size_type last_char_pos = test.find_last_not_of("0123456789");
+		std::cout  << "last not of is " << last_char_pos   << std::endl;
 		std::string base = test.substr(0, last_char_pos + 1);
 		if (base == test) {
 			std::cout  << "Input is not an image sequence. " << OpenFileName << std::endl;		
